ab5bcdd7d8112b2ee4c470d71644c0967a2c2417
1 <zsaida747@gmail.com>
2026-03-25 14:58:07 +0300

    GS-2-2 new task2

--- src/Lesson2/lesson2.kt
+++ src/Lesson2/lesson2.kt
@@ -1,8 +1,9 @@
 fun main(){
     // Задача 1
     println("Введите свой возраст: ")
-    val my_age = readLine()!!.toInt()
-    val result = my_age >= AGE_OF_MAJORITY
+    val my_age = readln()
+    val age = my_age.toInt()
+    val result = age >= AGE_OF_MAJORITY
     println("Совершеннолетие достигнуто: $result")
 
 
