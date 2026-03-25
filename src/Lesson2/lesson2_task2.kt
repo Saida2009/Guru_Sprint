2bd3271ca0750be5c01a1354f115ee5c6eba486f
1 <zsaida747@gmail.com>
2026-03-25 15:04:19 +0300

    GS-2-2 Исправленный вариант 2 задания

--- src/Lesson2/lesson2_task2.kt
+++ src/Lesson2/lesson2_task2.kt
@@ -1,6 +1,7 @@
 fun main(){
     println("Введите свой возраст: ")
-    val age = readLine()!!.toInt()
+    val my_age = readln()
+    val age = my_age.toInt()
 
     if(age >= 18){
         println("Добро пожаловать!")
