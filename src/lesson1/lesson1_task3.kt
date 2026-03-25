d1a4b0f4afe55a644593d496396827ffcb839844
1 <zsaida747@gmail.com>
2026-03-25 09:52:27 +0300

    GS-1-3 new task

---
+++ src/lesson1/lesson1_task3.kt
@@ -0,0 +1,7 @@
+package lesson1
+fun main(){
+    val botName: String = "TelegramBot"
+    val version: Double = 1.2
+    println("Привет! Я $botName, моя версия - $version")
+    println("Сумма выражения 2 + 2 равна: ${2 + 2}")
+}
